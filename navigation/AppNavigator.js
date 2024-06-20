843f15ed01c3b3d3860beda295c6330e6c787f72
Leonardo Tiago <pequenotiago@gmail.com>
2024-06-20 01:45:38 +0100

    Estruturação do projeto e criação da interface de carregamento

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from '../screens/SplashScreen';
+import MainScreen from '../screens/MainScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+    return (
+        <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+    );
+};
+
+export default AppNavigator;
---
+++ screens/SplashScreen.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+    useEffect( () => {
+        setTimeout(() => {
+            navigation.replace('Main');
+        }, 3000);
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/carbometrica.png')}
+                style={styles.carbometrica}
+            />
+            <Text style={styles.text}>Carbométrica, Ajudando você!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+    },
+    text: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#FF4500',
+    },
+    carbometrica: {
+        width: 100,
+        height: 100,
+    }
+});
+
+
+export default SplashScreen;
