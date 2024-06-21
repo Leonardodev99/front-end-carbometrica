eeb8bfc2ac6af942372d74056164907a6f0563fa
Leonardo Tiago <pequenotiago@gmail.com>
2024-06-21 09:29:41 +0100

    Finalizando a tela de login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
@@ -19,30 +19,38 @@ const LoginScreen = () => {
         }
     };
 
+    const handleForgotPassword = () => {
+        // Navegar para a tela de recuperação de senha
+        navigation.navigate('ForgotPassword');
+    };
+
     return (
         <View style={styles.container}>
-            
             <Image
                 source={require('../assets/carbometrica.png')}
                 style={styles.carbometrica}
             />
-            
-           
-            <Text style={styles.title}>Login</Text>
             <TextInput
-                style={styles.input}
+                style={[styles.input, { marginTop: 100 }]} // Adicionando margem superior ao primeiro campo de entrada
                 placeholder="Email"
+                placeholderTextColor="#FF4500" // Adicionando cor laranja ao placeholder
                 value={email}
                 onChangeText={setEmail}
             />
             <TextInput
-                style={styles.input}
+                style={[styles.input, {marginTop: 30}]}
                 placeholder="Password"
+                placeholderTextColor="#FF4500" // Adicionando cor laranja ao placeholder
                 secureTextEntry
                 value={password}
                 onChangeText={setPassword}
             />
-            <Button title="Login" onPress={handleLogin} />
+            <TouchableOpacity onPress={handleForgotPassword}>
+                <Text style={styles.forgotPasswordText}>ESQUECEU SUA SENHA?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>ENTRAR</Text>
+            </TouchableOpacity>
             {message ? <Text style={styles.message}>{message}</Text> : null}
         </View>
     );
@@ -53,24 +61,49 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         resizeMode: 'contain',
+        alignSelf: 'center',
+        marginBottom: 20,
+        marginTop: 40, // Adicionando margem superior para mover a imagem para o topo
     },
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start', // Alinhando o conteúdo ao topo
         padding: 16,
         backgroundColor: '#FFFFFF',
     },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
-        textAlign: 'center',
-    },
     input: {
         height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
+        borderColor: 'orange',
+        borderWidth: 0, // Removendo todas as bordas
+        borderBottomWidth: 1, // Adicionando apenas a borda inferior
         marginBottom: 12,
         padding: 10,
+        width: '40%', // Reduzido para 80%
+        alignSelf: 'center',
+        color: '#FF4500', // Adicionando cor laranja ao texto
+        textAlign: 'center', // Centralizando o texto dentro das caixas de entrada
+    },
+    forgotPasswordText: {
+        color: '#FF4500', // Adicionando cor laranja carregado ao texto
+        textAlign: 'center',
+        marginBottom: 20,
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: '#FFFFFF',
+        borderWidth: 2,
+        borderColor: '#0000FF',
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        width: '25%', // Reduzido o tamanho
+        alignSelf: 'center',
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#FF4500',
+        fontWeight: 'bold',
     },
     message: {
         marginTop: 20,
@@ -80,3 +113,5 @@ const styles = StyleSheet.create({
 });
 
 export default LoginScreen;
+
+
